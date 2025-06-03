a746a4dd989522c49ec02f669004be8a4f41c297
Nicolas <nicolasfigb@gmail.com>
2025-06-02 21:21:57 -0500

    Added button functionality

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let buttons = document.querySelectorAll(".button");
 let display = document.querySelector(".display");
+let clear = document.querySelector(".clear");
+let del = document.querySelector(".delete");
 let a = 0;
 let b = 0;
 
@@ -9,12 +11,47 @@ buttons.forEach((button) => {
   });
 });
 
+clear.addEventListener("click", () => {
+  number = "";
+  display.innerHTML = number;
+});
+
+del.addEventListener("click", () => {
+  number = number.substring(0, number.length - 1);
+  display.innerHTML = number;
+});
+
 let number = "";
 
 function capture(pressed) {
-  number += pressed;
-  console.log(number);
-  display.innerHTML = number;
+  if (number.length < 10) {
+    number += pressed;
+    display.innerHTML = number;
+  }
+  switch (pressed) {
+    case "+":
+      number = "";
+      display.innerHTML = number;
+      break;
+    case "-":
+      number = "";
+      display.innerHTML = number;
+      break;
+    case "*":
+      number = "";
+      display.innerHTML = number;
+      break;
+    case "/":
+      number = "";
+      display.innerHTML = number;
+      break;
+    case "=":
+      number = "";
+      display.innerHTML = number;
+      break;
+    case ".":
+      break;
+  }
 }
 
 function subtract(a, b) {
