00baab755a679f56a196756b22c48a13dcb509dc
Nicolas <nicolasfigb@gmail.com>
2025-06-03 22:40:12 -0500

    Added operations basic logic

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ let buttons = document.querySelectorAll(".button");
 let display = document.querySelector(".display");
 let clear = document.querySelector(".clear");
 let del = document.querySelector(".delete");
+
+// Numbers and operator used
 let a = 0;
 let b = 0;
 let operator = "";
+// Number captured from input as string
+let number = "";
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -22,8 +26,6 @@ del.addEventListener("click", () => {
   display.innerHTML = number;
 });
 
-let number = "";
-
 function capture(pressed) {
   if (number.length < 10) {
     number += pressed;
@@ -31,30 +33,33 @@ function capture(pressed) {
   }
   switch (pressed) {
     case "+":
-      operator = pressed;
-      number = "";
-      display.innerHTML = number;
+      captureOperator(pressed);
       break;
     case "-":
-      operator = pressed;
-      number = "";
-      display.innerHTML = number;
+      captureOperator(pressed);
       break;
     case "*":
-      operator = pressed;
-      number = "";
-      display.innerHTML = number;
+      captureOperator(pressed);
       break;
     case "/":
-      operator = pressed;
-      number = "";
-      display.innerHTML = number;
+      captureOperator(pressed);
       break;
     case "=":
+      if (operator !== "" && number.length > 0) {
+        b = Number(number.substring(number.length - 1));
+        number = "";
+        console.log(b);
+        operate(operator, a, b);
+      }
       break;
     case ".":
       break;
   }
+}
+
+function add(a, b) {
+  console.log(a + b);
+  return a + b;
 }
 
 function subtract(a, b) {
@@ -73,15 +78,28 @@ function divide(a, b) {
   }
 }
 
+function captureOperator(pressed) {
+  if (number.length > 0) {
+    a = Number(number.substring(0, number.length - 1));
+    operator = pressed;
+    number = "";
+    display.innerHTML = number;
+  }
+}
+
 function operate(operator, a, b) {
   switch (operator) {
     case "+":
+      add(a, b);
       break;
     case "-":
+      subtract(a, b);
       break;
     case "*":
+      multiply(a, b);
       break;
     case "/":
+      divide(a, b);
       break;
   }
 }
